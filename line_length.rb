9302064a7ba9fb1309a770296bcdc6f5d5d892b6
Bilal Khan <bilal@advancetogo.com>
2016-05-15 22:13:02 +0100

    Add line length linters

---
+++ line_length.rb
@@ -0,0 +1,22 @@
+module SCSSLint
+  # Checks for lines longer than a maximum number of columns.
+  class Linter::LineLength < Linter
+    include LinterRegistry
+
+    MSG = 'Line is too long. [%d/%d]'.freeze
+
+    def visit_root(_node)
+      max_length = config['max']
+      dummy_node = Struct.new(:line)
+
+      engine.lines.each_with_index do |line, index|
+        next if line.length <= max_length
+
+        add_lint(
+          dummy_node.new(index + 1),
+          format(MSG, line.length, max_length)
+        )
+      end
+    end
+  end
+end
